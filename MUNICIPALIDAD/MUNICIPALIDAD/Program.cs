d222e7ca42cd7220a74700535d9eab8debd6a9ba
tomas <tmgarcia@miuandes.cl>
2018-05-22 23:01:54 -0400

    actualizacion2

--- MUNICIPALIDAD/MUNICIPALIDAD/Program.cs
+++ MUNICIPALIDAD/MUNICIPALIDAD/Program.cs
@@ -12,6 +12,7 @@ namespace MUNICIPALIDAD
     {
         static void Main(string[] args)
         {
+            /*
             Type t1 = typeof(Person);
             Console.WriteLine("Person");
             //t1.GetProperties() t1.GetMethods()
@@ -33,7 +34,7 @@ namespace MUNICIPALIDAD
             {
                 Console.WriteLine("{0}", i);
             }
-            
+            */
 
             List<Person> personas = new List<Person>();
             List<Car> cares = new List<Car>();
@@ -51,32 +52,30 @@ namespace MUNICIPALIDAD
                 string r1 = Console.ReadLine();
                 Console.WriteLine("LAST NAME");
                 string r2 = Console.ReadLine();
-                Console.WriteLine("BIRTH DATE"); //DATE TIME
+                fecha:
+                Console.WriteLine("BIRTH DATE (AAAA/MM/DD)"); //DATE TIME
                 string r3 = Console.ReadLine();
-                DateTime birth = Convert.ToDateTime(r3);
                 try
                 {
                     DateTime birth = Convert.ToDateTime(r3);
                     
                 }
-                catch(FormatException)
+                catch(Exception e)
                 {
-                    Console.WriteLine("ese no es el formato, debe ser 01/01/1111"); 
+                    Console.WriteLine("ese no es el formato, debe ser (1111/11/30)");
+                    goto fecha;
                 }
-                
-
+                DateTime b2 = Convert.ToDateTime(r3);
                 Console.WriteLine("ADDRESS");
                 string r4 = Console.ReadLine();
-
                 Console.WriteLine("RUT");
                 string r5 = Console.ReadLine();
-
                 Console.WriteLine("PARENT 1");
                 string r6 = Console.ReadLine();
                 Console.WriteLine("PARENT 2");
                 string r7 = Console.ReadLine();
 
-                Person p1 = new Person(r1,r2,birth, null ,r5,null,null );
+                Person p1 = new Person(r1,r2,b2, null ,r5,null,null );
                 personas.Add(p1);
 
             }
@@ -87,20 +86,46 @@ namespace MUNICIPALIDAD
                 string r1 = Console.ReadLine();
                 Console.WriteLine("MODEL");
                 string r2 = Console.ReadLine();
+                otravez:
                 Console.WriteLine("YEAR"); //INT
                 string r3 = Console.ReadLine();
-                Console.WriteLine("OWNER"); //INT
+                try
+                {
+                    int i = Convert.ToInt32(r3);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Solo numeros");
+                    goto otravez;
+                }
+                Int32 r33 = Convert.ToInt32(r3);
+                Console.WriteLine("OWNER");
                 string r7 = Console.ReadLine();
                 Console.WriteLine("LISENCE PLATE");
                 string r4 = Console.ReadLine();
+                esenomen:
                 Console.WriteLine("SEATBELTS"); // INT
                 string r5 = Console.ReadLine();
-                Console.WriteLine("DIESEL"); //BOOL
+                try
+                {
+                    Int32 i1 = Convert.ToInt32(r5);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Solo numeros");
+                    goto esenomen;
+                }
+                Int32 r55 = Convert.ToInt32(r5);
+
+                Console.WriteLine("DIESEL: (1)si, (2)no"); //BOOL
                 string r6 = Console.ReadLine();
-
-                //Car c1 = new Car();
-                //cares.Add(c1);
-
+                bool di = false;
+                if (r6 == "1")
+                {
+                    di = true;
+                }
+                Car c1 = new Car(r1,r2,r33,null,r4,r55,di);
+                cares.Add(c1);
             }
 
             if (res == "3")
@@ -108,32 +133,72 @@ namespace MUNICIPALIDAD
                 Console.Clear();
                 Console.WriteLine("STREET");
                 string r1 = Console.ReadLine();
+                esenomen1:
                 Console.WriteLine("NUMBER"); //INT32
                 string r2 = Console.ReadLine();
+                try
+                {
+                    Int32 i2 = Convert.ToInt32(r2);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Solo numeros");
+                    goto esenomen1;
+                }
+                Int32 r22 = Convert.ToInt32(r2);
                 Console.WriteLine("COMMUNE");
                 string r3 = Console.ReadLine();
                 Console.WriteLine("CITY");
                 string r4 = Console.ReadLine();
                 Console.WriteLine("OWNER"); //person (?)
                 string r5 = Console.ReadLine();
+                esenomen3:
                 Console.WriteLine("YEAR OF CONSTRUCTION"); //INT
                 string r6 = Console.ReadLine();
                 Console.WriteLine("BEDROOMS"); //INT
                 string r7 = Console.ReadLine();
                 Console.WriteLine("BATHROOMS"); //INT
                 string r8 = Console.ReadLine();
-                Console.WriteLine("BACKYARD"); //bOOL
+                try
+                {
+                    Int32 i3 = Convert.ToInt32(r6);
+                    Int32 i4 = Convert.ToInt32(r7);
+                    Int32 i5 = Convert.ToInt32(r8);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Solo numeros");
+                    goto esenomen3;
+                }
+                Int32 r66 = Convert.ToInt32(r6);
+                Int32 r77 = Convert.ToInt32(r7);
+                Int32 r88 = Convert.ToInt32(r8);
+                Console.WriteLine("BACKYARD: (1)si(2)no"); //bOOL
                 string r9 = Console.ReadLine();
-
-                //Address a1 = new Address(r1);
-                //addresses.Add(a1);
+                bool back = true;
+                if (r9 == "2")
+                {
+                    back = false;
+                }
+                Console.WriteLine("POOL: (1)si(2)no"); //bOOL
+                string r10 = Console.ReadLine();
+                bool pul = true;
+                if (r9 == "2")
+                {
+                    pul = false;
+                }
+                Address a1 = new Address(r1,r22,r3,r4,null,r66,r77,r88,back,pul);
+                addresses.Add(a1);
             }
             
             else
             {
+                imp1(personas);
+                imp2(cares);
+                imp3(addresses);
+                Console.ReadLine();
                 goto inicio;
             }
-
 
 
             Console.ReadLine();
@@ -143,21 +208,36 @@ namespace MUNICIPALIDAD
         {
             foreach (var t in list)
             {
-                Console.WriteLine(t);
+                Console.WriteLine("First Name: "+ t.First_name);
+                Console.WriteLine("Last Name: "+ t.Last_name);
+                Console.WriteLine("Birth: "+ t.Birth_date);
+                Console.WriteLine("rut: "+ t.Rut);
             }
         }
         public static void imp2(List<Car> list)
         {
             foreach (var t in list)
-            {
-                Console.WriteLine(t);
+            { 
+                Console.WriteLine("brand " + t.Brand);
+                Console.WriteLine("model "+ t.Model);
+                Console.WriteLine("year "+ t.Year);
+                Console.WriteLine("license plate "+ t.License_plate);
+                Console.WriteLine("seatbelts "+ t.Seatbelts);
+                Console.WriteLine("diesel "+ t.Diesel);
             }
         }
         public static void imp3(List<Address> list)
         {
             foreach (var t in list)
             {
-                Console.WriteLine(t);
+                Console.WriteLine("Street: "+ t.Street);
+                Console.WriteLine("number: "+ t.Number);
+                Console.WriteLine("commune: "+ t.Commune);
+                Console.WriteLine("city: "+ t.City);
+                Console.WriteLine("Year of contuction: "+ t.Year_of_contruction);
+                Console.WriteLine("bedrooms: "+ t.Bedrooms);
+                Console.WriteLine("bathrooms: "+ t.Bathrooms);
+                Console.WriteLine("backyard: "+ t.Backyard);
             }
         }
     }   
